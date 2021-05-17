9a7501d2a784c5ed2c86143062432796717d9351
bukit3point0 <bukit3point0@gmail.com>
2021-05-17 18:29:55 -0400

    auth-middleware done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,54 +1,62 @@
 const User = require('../users/users-model')
 
-/*
-  If the user does not have a session saved in the server
-
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
-function restricted() {
-
+function restricted(req, res, next) {
+  const {username, password} = req.body
+  User.findBy({username})
+  .then(([user]) => {
+      if (user && bcrpyt.compareSync(password, user.password)) {
+          console.log(req.session)
+          req.session.user = user
+          res.json({
+              message: `${user.username} found!`
+          })
+      } else {
+      next({
+          status: 401, 
+          message: `You shall not pass!`
+      })
+      }
+  })
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  const {username} = req.body.username
+  User.findBy(username)
+  .then(user => {
+    if(!user) {
+      next()
+    } else {
+      res.status(422).json({
+        message: 'Username taken'
+      })
+    }
+  })
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  const {username} = req.body.username
+  User.findBy(username)
+  .then(user => {
+    if(user) {
+      next()
+    } else {
+      res.status(401).json({
+        message: `Invalid credentials`
+      })
+    }
+  })
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+function checkPasswordLength(req, res, next) {
+  const {password} = req.body.password
+  if (!password || password < 3) {
+    res.status(422).json({
+      message: `Password must be longer than 3 chars`
+    })
+  } else {
+    next()
   }
-*/
-function checkPasswordLength() {
-
 }
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
 
 module.exports = {
   restricted,
